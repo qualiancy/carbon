8b58415670c816f6a6f8af97ed36ff66c50d2f8c
Jake Luer <jake.luer@gmail.com>
2012-01-22 03:54:18 -0500

    updated example

--- examples/basic.js
+++ examples/basic.js
@@ -5,14 +5,11 @@ var server = http.createServer().listen(8000)
   , balancer = new carbon.Stack(server);
 
 balancer.use(function (req, res, next) {
-  res.writeHead(200);
-  res.write('Hello World');
-  res.end();
   next(9000);
 });
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello World');
+  res.write('Hello Universe');
   res.end();
 }).listen(9000);
