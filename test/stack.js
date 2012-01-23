be27a832623e115aae5819729a4996f8cb88f5e2
Jake Luer <jake.luer@gmail.com>
2012-01-23 05:41:57 -0500

    starting tests

---
+++ test/stack.js
@@ -0,0 +1,29 @@
+var should = require('chai').should()
+  , http = require('http');
+
+var carbon = require('..');
+
+describe('Carbon#Stack', function () {
+
+  it('should have a valid version', function () {
+    carbon.version.should.match(/^\d+\.\d+\.\d$/);
+  });
+
+  it('should be an event emitter', function (done) {
+    var h = http.createServer().listen(6789)
+      , s = new carbon.Stack(h);
+    s.on('test event', function (opts) {
+      opts.should.eql({ hello: 'universe' });
+      done();
+    });
+    s.emit('test event', { hello: 'universe' });
+  });
+
+  it('should throw an error if no server provided' , function () {
+    function newServer () {
+      var s = new carbon.Stack();
+      return s;
+    }
+    should.throw(newServer, Error);
+  });
+});
