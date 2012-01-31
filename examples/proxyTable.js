232a1b8f7cbdd03d7592e575a83d7d7de74eb7a3
Jake Luer <jake.luer@gmail.com>
2012-01-31 05:47:07 -0500

    proxy table and example

--- examples/proxyTable.js
+++ examples/proxyTable.js
@@ -1,11 +1,25 @@
-var carbon = require('..')
+var http = require('http')
+  , carbon = require('..')
   , proxy = carbon.listen(8080);
 
 var table = [
-    { hostname: 'qualiancy.com'
+    { hostname: 'proxyone.com:8080'
     , port: 8000 }
-  , { hostname: 'alogicalparadox.com'
-    , port: 1227 }
+  , { hostname: 'proxytwo.com:8080'
+    , dest: 'localhost:1227' }
 ];
 
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'content-type': 'text/plain' });
+  res.write('proxy server one');
+  res.end();
+}).listen(8000);
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'content-type': 'text/plain' });
+  res.write('proxy server two');
+  res.end();
+}).listen(1227);
+
 proxy.use(carbon.proxyTable(table));
+console.log('Proxy server active on port %d', proxy.server.address().port);
