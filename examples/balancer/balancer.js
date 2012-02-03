dab30aad459c8b4945864635cf9cdf85341b2a64
Jake Luer <jake.luer@gmail.com>
2012-02-03 05:14:50 -0500

    no downtime restarts!

--- examples/balancer/balancer.js
+++ examples/balancer/balancer.js
@@ -1,6 +1,12 @@
-var carbon = require('../..')
+var http = require('http')
+  , carbon = require('../..')
 
-var proxy = carbon.listen(4567);
+var server = module.exports = http.createServer()
+  , proxy = carbon.attach(server);
 
+// make a change to universe and it will reload :)
 proxy.use(carbon.balancer(__dirname + '/universe.js'));
 
+if (require.main === module) {
+  server.listen(8080);
+}
