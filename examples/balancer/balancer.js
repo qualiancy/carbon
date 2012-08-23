e44dac2de45fc502067f39275f4acc0cffbd5b6d
Jake Luer <jake.luer@gmail.com>
2012-08-23 17:48:05 -0400

    watch is no longer default for balancer, update examples

--- examples/balancer/balancer.js
+++ examples/balancer/balancer.js
@@ -5,10 +5,10 @@ var server = module.exports = http.createServer()
   , proxy = carbon.listen(server);
 
 // make a change to world and it will reload :)
-proxy.use(carbon.balancer('*', __dirname + '/world.js'));
+proxy.use(carbon.balancer('*', __dirname + '/world.js', { watch: true }));
 
 // make a change to universe and it will reload :)
-proxy.use(carbon.balancer('*', __dirname + '/universe.js'))
+proxy.use(carbon.balancer('*', __dirname + '/universe.js', { watch: true }))
 
 if (require.main === module) {
   server.listen(8080);
