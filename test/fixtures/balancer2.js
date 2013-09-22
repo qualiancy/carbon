98cd826f2f70d6be816a7e57507e37300bc1953b
Jake Luer <jake@alogicalparadox.com>
2013-09-22 03:06:03 -0400

    tests: remove all the old

--- test/fixtures/balancer2.js
+++
@@ -1,8 +0,0 @@
-module.exports = require('http').createServer(function (req, res) {
-  switch (req.url) {
-    case '/inst':
-      res.writeHeader(200, { 'content-type' : 'text/plain' });
-      res.end('2');
-      break;
-  }
-});
